6681ed00f7f2e22040c08ee138d2740d7702979a
Ícaro Silva <icarogabrielsilva2019@gmail.com>
2021-10-21 01:08:31 -0300

    Added updated script

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 main() {
   #Initialization
-  $_warnings=""
-  $_loop=1
+  _warnings=""
+  _loop=1
   install_figlet_themagic
   
   #Main Display
@@ -23,8 +23,7 @@ main() {
   #Warning display
   tsunami_art
   echo "Processo finalizado"
-  if [[ -z $_warnings ]];
-  then
+  if [[ -n $_warnings ]]; then
     echo "Warnings: $_warnings"
   else
     echo "Tudo ocorreu bem"
@@ -39,32 +38,32 @@ main() {
 }
 
 ask_clone() {
-  while [[ "$_loop" -eq 1 ]];
+  while [[ $_loop -eq 1 ]];
   do
     echo "Deseja clonar os repositórios[Tsunami/Wavez]? (y/n)"
     read _option;
     case $_option in
       "y")
          cloning_repos
-         $_loop=0
+         _loop=0
          ;;
       "Y")
          cloning_repos
-         $_loop=0
+         _loop=0
          ;;
       "s")
          cloning_repos
-         $_loop=0
+         _loop=0
          ;;
       "S")
          cloning_repos
-         $_loop=0
+         _loop=0
          ;;
       "n")
-         $_loop=0
+         _loop=0
          ;;
       "N")
-         $_loop=0
+         _loop=0
          ;;
       *)
          echo "Opção inválida, tente novamente"
@@ -109,11 +108,11 @@ basic_apt_checkout() {
 install_java() {
   if which java
   then
-    $_warnings="$_warnings Java já instalado configure manualmente;"
+    _warnings="$_warnings Java já instalado configure manualmente;"
   else
     if [ $# -gt 1 ];
     then
-      $_java_version=$1
+      _java_version=$1
     else
       echo "Selecione qual versão do Java gostaria de instalar"
       echo "1) Java 8"
@@ -121,19 +120,19 @@ install_java() {
       read _option;
       case $_option in
         "1")
-	   $_java_version=openjdk-8-jdk
-	   $_java_path=/usr/lib/jvm/java-8-openjdk-amd64
+	   _java_version=openjdk-8-jdk
+	   _java_path=/usr/lib/jvm/java-8-openjdk-amd64
 	   ;;
         "2")
-           $_java_version=openjdk-11-jdk
-	   $_java_path=/usr/lib/jvm/java-11-openjdk-amd64
+           _java_version=openjdk-11-jdk
+	   _java_path=/usr/lib/jvm/java-11-openjdk-amd64
 	   ;;
       esac
     fi
     echo "Instalando o Java"
     if ! apt install $_java_version -y
     then
-      $_warnings="$_warnings Não foi possível instalar o Java;"
+      _warnings="$_warnings Não foi possível instalar o Java;"
     else
       echo "Java Instalado"
     fi
@@ -147,7 +146,7 @@ configure_java() {
   then
     if ! find $_java_path
     then
-      $_warnings="$_warnings Caminho do java não encontrado, configure manualmente;"
+      _warnings="$_warnings Caminho do java não encontrado, configure manualmente;"
     else
       echo "PATH=$_java_path/bin:$PATH" > /etc/environment
       echo "JAVA_HOME=$_java_path" >> /etc/environment
@@ -169,7 +168,7 @@ install_docker() {
     apt install docker-ce docker-ce-cli containerd.io -y
     if ! docker -v
     then
-      $_warnings="$_warnings Não foi possivel instalar o Docker;"
+      _warnings="$_warnings Não foi possivel instalar o Docker;"
     else
       echo "Docker instalado"
     fi
@@ -185,7 +184,7 @@ configure_docker() {
     echo "Configurando o Docker (postgres)"
     if ! docker pull postgres
     then
-      $_warnings="$_warnings Não foi possivel configurar o Docker (postgres);"
+      _warnings="$_warnings Não foi possivel configurar o Docker (postgres);"
     else
       echo "Docker configurado (postgres)"
     fi
@@ -207,7 +206,7 @@ install_utilities() {
 install_maven() {
   if ! apt install maven -y
   then
-    $_warnings="$_warnings Não foi possível instalar o Maven;"
+    _warnings="$_warnings Não foi possível instalar o Maven;"
   else
     echo "Maven instalado"
   fi
@@ -216,27 +215,32 @@ install_maven() {
 install_git() {
   if ! apt install git -y
   then
-    $_warnings="$_warnings Não foi possível instalar o Git;"
+    _warnings="$_warnings Não foi possível instalar o Git;"
   else
     echo "Git instalado"
   fi
 }
 
 cloning_repos() {
-  cd ~
-  mkdir dextra-repos
-  cd dextra-repos
-  if ! git clone https://github.com/dextra/tsunami.git
-  then
-    echo "Não foi possível clonar o repositório do tsunami"
+  read -p "username: " _username
+  _key=$(find /home -name id_rsa.pub)
+  if [[ -f $_key ]]; then
+    sudo -H -u $_username bash -c "mkdir dextra-repos && chmod 751 dextra-repos"
+    cd dextra-repos
+    if ! sudo -H -u $_username bash -c "git clone git@github.com:dextra/tsunami.git"
+    then
+      echo "Não foi possível clonar o repositório do tsunami"
+    else
+      echo "Repositório do tsunami clonado"
+    fi
+    if ! sudo -H -u $_username bash -c "git clone git@github.com:dextra/my-way.git"
+    then
+      echo "Não foi possível clonar o repositório do wavez"
+    else
+      echo "Repositório do wavez clonado"
+    fi
   else
-    echo "Repositório do tsunami clonado dentro da home"
-  fi
-  if ! git clone https://github.com/dextra/my-way.git
-  then
-    echo "Não foi possível clonar o repositório do wavez"
-  else
-    echo "Repositório do wavez clonado dentro da home"
+    echo "Certifique-se de ter configurado a chave SSH corretamente"
   fi
 }
 
